5d8e3d4ffd33c30ecc89aa13c44515d36efe1f1c
unknown1-name <hatunknown3@gmail.com>
2024-11-23 20:03:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ app.use(cors({
 }));
 
 
-mongoose.connect("mongodb+srv://abosalah:Abo_salah100@cluster0.vuac5.mongodb.net/material_sorting_game?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://abosalah:Abo_salah100@cluster0.vuac5.mongodb.net/material_sorting_game?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+}).then(() => console.log('Database connected successfully'))
+.catch((err) => console.log('Error connecting to database', err));
 
 app.get('/', (req, res) => {
     res.send('Hello from Express server!');
